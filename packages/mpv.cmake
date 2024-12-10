a9ceece0879ff7fdadee3e0b7515f9eef36f245a
fyyhhGit <56672700+fyyhhGit@users.noreply.github.com>
2024-12-10 13:02:30 +0800

    Update mpv.cmake

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -15,7 +15,6 @@ ExternalProject_Add(mpv
         shaderc
         spirv-cross
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
-    GIT_TAG v0.36.0
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
